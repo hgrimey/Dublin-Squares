0ffeebf90a2df46f0cdf8218ffeb4d87fb8848dd
gillb111 <bowes.gillian@gmail.com>
2018-11-30 10:53:13 +0000

    hostspots animation

--- js/index4.js
+++ js/index4.js
@@ -214,7 +214,7 @@ function buildHouseHTML(house) {
     if (fourthImage != null) {
         html += '<div class="imgHouse"><img src="' + getImageUrl(fourthImage.houseImageURL) + '"/></div>\r\n'
     }
-    debugger;
+    //debugger;
     html += buildTimeline(timeline);
 
     html += '</div>'
@@ -232,6 +232,7 @@ function loadHouse(id, callback) {
     });
 }
 
+//animated SVGs on each house page
 function createHouseSVG() {
 
     var url = $('#mapViewport .imgHouse').data('img-url');
